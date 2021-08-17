175d61a838dfec76750faeddfe63c3832a3f0d25
JosueFlx <josueflx1999@gmail.com>
2021-08-17 16:28:17 -0300

    Adicionado data e hora com observables

--- src/app/componentes/lancamentos/lancamentos.component.ts
+++ src/app/componentes/lancamentos/lancamentos.component.ts
@@ -1,27 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { LancamentoService } from 'src/app/servicos/lancamento.service';
 import { Lancamento } from 'src/app/models/lancamentos.models';
+import { DataHoraService } from 'src/app/servicos/data-hora.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lancamentos',
   templateUrl: './lancamentos.component.html',
   styleUrls: ['./lancamentos.component.css']
 })
-export class LancamentosComponent implements OnInit {
+export class LancamentosComponent implements OnInit, OnDestroy {
 
-  lancamentos: Lancamento[] = []
+  lancamentos: Lancamento[] = [];
+  dataHoraAtual = "";
+  dataTempoReal = "";
+  dataHoraAtualSub: Subscription | undefined;
+  dataTempoRealSub: Subscription | undefined;
 
-  constructor(private lancamentoService: LancamentoService) { }
+
+  constructor(private lancamentoService: LancamentoService,
+              private dataHoraService: DataHoraService) { }
+  ngOnDestroy(): void {
+    this.dataHoraAtualSub?.unsubscribe();
+    this.dataTempoRealSub?.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.lancamentoService.listarTodos().subscribe(
       dados => this.lancamentos = dados.data.content,
       () => alert('Erro obtendo lançamentos.')
     );
+    this.dataHoraAtualSub = this.dataHoraService.dataHora.subscribe(
+      dataHora => this.dataHoraAtual = dataHora
+    );
+    this.dataHoraService.dataHoraTempoReal.subscribe(
+      dataHora => this.dataTempoReal = dataHora
+    );
   }
 
   urlLocalizacao(localizacao: string) {
     return "https://www.google.com/maps/search/?api=1&query=" + localizacao;
+  }
+
+  atualizarDataHora() {
+    this.dataHoraService.atualizarDataHora();
   }
 
 }
--- src/app/servicos/data-hora.service.ts
+++ src/app/servicos/data-hora.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, interval } from "rxjs";
+import { map, share } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DataHoraService {
 
+  dataHora = new BehaviorSubject(this.obterDataHoraAtual());
+  dataHoraTempoReal = interval(500).pipe(
+    map(() => this.obterDataHoraAtual()), share()
+  );
+
   constructor() { }
+
+  obterDataHoraAtual() {
+    const data = new Date();
+    return data.toLocaleString("pt-BR");
+  }
+
+  atualizarDataHora() {
+    this.dataHora.next(this.obterDataHoraAtual());
+  }
+  
 }
